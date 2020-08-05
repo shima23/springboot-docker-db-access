4ce8c026eb8d700a94a0eaf2cd75f2a603ae9547
tmk <manaka123480@yahoo.co.jp>
2020-08-05 15:43:40 +0900

    first commit

---
+++ src/main/kotlin/demo/example/dockerdbaccess/DockerdbaccessApplication.kt
@@ -0,0 +1,11 @@
+package demo.example.dockerdbaccess
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DockerdbaccessApplication
+
+fun main(args: Array<String>) {
+	runApplication<DockerdbaccessApplication>(*args)
+}
---
+++ src/test/kotlin/demo/example/dockerdbaccess/DockerdbaccessApplicationTests.kt
@@ -0,0 +1,13 @@
+package demo.example.dockerdbaccess
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DockerdbaccessApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
